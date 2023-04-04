f7395ffd6968199a2cb142a2aa08397c6b2dd86e
Egor <egor.martinenko01@gmail.com>
2023-04-04 16:48:27 +0300

    some comment

---
+++ dist/script/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.feedback__group').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: true,
+    });
+    
+});
